20b0758c38aff5065f30a4e447655a69fa5522b0
toddxxx <1475225899@qq.com>
2020-06-27 12:06:47 +0800

    首页

---
+++ js/personal.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 14752 on 2020-06-27.
+ */
+var personal = new Vue({
+    el:'#personal',
+    data:{
+        id:'201830663124',
+        name:'toddxxx'
+    },
+    methods:{
+
+    }
+})
+
+function logout(){
+    personal.id=''
+}
+
+
