95b846ac19f22bb6751f11b3930944c2f50aca55
toddxxx <1475225899@qq.com>
2020-07-11 12:14:40 +0800

    求物品页去掉菜单，优化js

--- js/askItem.js
+++ js/askItem.js
@@ -15,60 +15,24 @@ var askItem = new Vue({
         chooseIndex:-1
     },
     methods:{
-        change_classify:function(index){
-            //index是classify的下标，发送请求，获取itemList信息
-            if(this.chooseIndex==index) return;
-            this.chooseIndex=index;
-            this.alreadyGot=0;
-            this.count=0;
-            var self=this;
-            if(index==0) {
-                //获取全部类型的全部物品信息，
-                axios.get(localStorage.serverUrl+'commodity/getRequiredCommodities')
-                    .then(function (response) {
-                        self.itemList=response.data.list; self.count=response.data.count;
-                        if(response.data.list.length) self.alreadyGot+=response.data.list.length;
-                        //console.log(response);
-                        console.log( self.itemList);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
-            }
-            else{
-                //获取全部类型的全部物品信息，
-                axios.get(localStorage.serverUrl+'commodity/getRequiredCommodities')
-                    .then(function (response) {
-                        self.itemList=response.data.list; self.count=response.data.count;
-                        if(response.data.list.length) self.alreadyGot+=response.data.list.length;
-                        //console.log(response);
-                        console.log( self.itemList);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
 
-
-                //否则获取某一类型的全部物品信息
-                //axios.get(localStorage.serverUrl+'queryRequiredCommoditiesByType?commodity_type='+self.classify[self.chooseIndex] )
-                //    .then(function (response) {
-                //        self.itemList=response.data ;
-                //        if(response.data.length) self.alreadyGot+=response.data.length;
-                //        console.log(response);
-                //    })
-                //    .catch(function (error) {
-                //        // console.log(error);
-                //    });
-            }
-
-        },
         to_detail:function(id){
             window.location.href="itemDetail.html?open_type=ask&id="+id
         }
     },
     created:function(){
-        //下标为0发送请求
-        this.change_classify(0)
+        var self=this;
+        //获取全部类型的全部物品信息，
+        axios.get(localStorage.serverUrl+'commodity/getRequiredCommodities')
+            .then(function (response) {
+                self.itemList=response.data.list; self.count=response.data.count;
+                if(response.data.list.length) self.alreadyGot+=response.data.list.length;
+                //console.log(response);
+                console.log( self.itemList);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
 })
